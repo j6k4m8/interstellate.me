e0d7df1eccf249273ae5f778d5d570be6930edea
Jordan Matelsky <j6k4m8@gmail.com>
2017-04-27 08:02:01 -0400

    Committing...

---
+++ client/stories/real/scripts.js
@@ -0,0 +1,8 @@
+Template.story_real.rendered = function() {
+    $.Scrollax();
+
+    $('.fade-in-2s').fadeIn(2000);
+    setTimeout(function() {
+        $('.fade-in-2s-w05').fadeIn(2000);
+    }, 500);
+}
