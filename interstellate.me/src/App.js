300784d455968e84300deb3b302327add6282271
Jordan Matelsky <j6k4m8@gmail.com>
2017-05-13 18:53:54 -0400

    yay

--- interstellate.me/src/App.js
+++ interstellate.me/src/App.js
@@ -5,10 +5,7 @@ import {
 } from 'react-router-dom'
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBar from 'material-ui/AppBar';
 import {GridList, GridTile} from 'material-ui/GridList';
-import IconButton from 'material-ui/IconButton';
-import Subheader from 'material-ui/Subheader';
 
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
@@ -20,6 +17,61 @@ injectTapEventPlugin();
 import './index.css';
 
 import Story from './Story';
+
+class Loading extends Component {
+    render() {
+        // <!-- By Sam Herbert (@sherb), for everyone. More @ http://goo.gl/7AJzbL -->
+        return (
+            <div style={{
+                    width: "2em",
+                    margin: "auto",
+                    marginTop: "10vh"
+                }}>
+                <svg width="360" height="360" viewBox="0 0 90 90" xmlns="http://www.w3.org/2000/svg" stroke="#fff">
+                    <g fill="none" fill-rule="evenodd" transform="translate(1 1)" stroke-width="2">
+                        <circle cx="22" cy="22" r="6" stroke-opacity="0">
+                            <animate attributeName="r"
+                                begin="1.5s" dur="3s"
+                                values="6;22"
+                                calcMode="linear"
+                                repeatCount="indefinite" />
+                            <animate attributeName="stroke-opacity"
+                                begin="1.5s" dur="3s"
+                                values="1;0" calcMode="linear"
+                                repeatCount="indefinite" />
+                            <animate attributeName="stroke-width"
+                                begin="1.5s" dur="3s"
+                                values="2;0" calcMode="linear"
+                                repeatCount="indefinite" />
+                        </circle>
+                        <circle cx="22" cy="22" r="6" stroke-opacity="0">
+                            <animate attributeName="r"
+                                begin="3s" dur="3s"
+                                values="6;22"
+                                calcMode="linear"
+                                repeatCount="indefinite" />
+                            <animate attributeName="stroke-opacity"
+                                begin="3s" dur="3s"
+                                values="1;0" calcMode="linear"
+                                repeatCount="indefinite" />
+                            <animate attributeName="stroke-width"
+                                begin="3s" dur="3s"
+                                values="2;0" calcMode="linear"
+                                repeatCount="indefinite" />
+                        </circle>
+                        <circle cx="22" cy="22" r="8">
+                            <animate attributeName="r"
+                                begin="0s" dur="1.5s"
+                                values="6;1;2;3;4;5;6"
+                                calcMode="linear"
+                                repeatCount="indefinite" />
+                        </circle>
+                    </g>
+                </svg>
+            </div>
+        )
+    }
+}
 
 
 class Home extends Component {
@@ -47,9 +99,21 @@ class Home extends Component {
             <div>
                 <h1 style={{
                         color: "white",
-                        margin: "2rem 4rem"
+                        margin: "2rem 4rem",
+                        fontFamily: "'Roboto Mono', monospace",
+                        letterSpacing: "0.1em"
                     }}>
-                    Interstellate_
+                    INTERSTELLATE_
+                    <p style={{
+                        marginLeft: "9rem",
+                        marginTop: "-0.3rem",
+                        fontSize: "0.8rem",
+                        color: "#dedede",
+                        letterSpacing: "1"
+                    }}>curated by <a href="https://twitter.com/caitvw" style={{
+                        color: "rgb(200, 230, 255)",
+                        textDecoration: "none"
+                    }}>@caitvw</a></p>
                 </h1>
                 { this.state.data.length ? (
                     <GridList
@@ -84,9 +148,9 @@ class Home extends Component {
                         ))}
                     </GridList>
                 ) : (
-                    <div style={{
-                        color: "white", margin: "4rem"
-                        }}>Loading...</div>
+				    <div>
+					    <Loading />
+				    </div>
                 ) }
             </div>
         )
